762f46333f1293cb3d9c0ebf27370ed20d96c888
Masamitsu MURASE <masamitsu.murase@gmail.com>
2013-12-28 20:31:33 +0900

    Modify 7zip command in test preparation.

--- spec/seven_zip_ruby_spec_helper.rb
+++ spec/seven_zip_ruby_spec_helper.rb
@@ -76,8 +76,8 @@ module SevenZipRubySpecHelper
 
     def prepare_seven_zip_file
       Dir.chdir(SAMPLE_FILE_DIR) do
-        my_system("7z a -bd \"#{SEVEN_ZIP_FILE}\" *")
-        my_system("7z a -bd \"-p#{SEVEN_ZIP_PASSWORD}\" \"#{SEVEN_ZIP_PASSWORD_FILE}\" *")
+        my_system("7z a -bd \"#{SEVEN_ZIP_FILE}\" * .*")
+        my_system("7z a -bd \"-p#{SEVEN_ZIP_PASSWORD}\" \"#{SEVEN_ZIP_PASSWORD_FILE}\" * .*")
       end
     end
 
